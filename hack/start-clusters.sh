5b14d0479645c9aa7b75ff8a7e721ffa3722db2d
Israel Blancas <iblancasa@gmail.com>
2023-12-18 17:31:02 +0100

    Fix deployment of OTEL ingress
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

--- hack/start-clusters.sh
+++ hack/start-clusters.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname ${BASH_SOURCE})
 
-set -ex
+set -e
 
 hub=${CLUSTER1:-hub}
 c1=${CLUSTER1:-cluster1}
@@ -10,8 +10,8 @@ c1=${CLUSTER1:-cluster1}
 hubctx="kind-${hub}"
 c1ctx="kind-${c1}"
 
-kind create cluster --name "${hub}"
-kind create cluster --name "${c1}"
+kind create cluster --name "${hub}" --config hub.yaml
+kind create cluster --name "${c1}"  --config cluster1.yaml
 
 clusteradm init --wait --context ${hubctx}
 joincmd=$(clusteradm get token --context ${hubctx} | grep clusteradm)
@@ -21,3 +21,6 @@ $(echo ${joincmd} --force-internal-endpoint-lookup --wait --context ${c1ctx} | s
 clusteradm accept --context ${hubctx} --clusters ${c1} --wait
 
 kubectl get managedclusters --all-namespaces --context ${hubctx}
+
+kubectl create -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.0.1/deploy/static/provider/kind/deploy.yaml --context kind-hub
+kubectl wait --for=condition=available deployment ingress-nginx-controller -n ingress-nginx --timeout=5m --context kind-hub
